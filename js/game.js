24af4b814c94829e700c5b60ba7a1ea725daf6ef
Riccosblogger <57557248+Riccosblogger@users.noreply.github.com>
2020-06-09 23:25:30 +0300

    add all file (version 1)

---
+++ js/game.js
@@ -0,0 +1,98 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
+
+// Звуковые файлы
+var fly = new Audio();
+var score_audio = new Audio();
+var gameOver = new Audio();
+
+fly.src = "audio/fly.mp3";
+score_audio.src = "audio/score.mp3";
+gameOver.src = "audio/gameover.mp3";
+
+var gap = 180;
+
+// При нажатии на какую-либо кнопку
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+ yPos -= 25;
+ fly.play();
+}
+
+function sleep(ms) {
+    ms += new Date().getTime();
+    while (new Date() < ms){}
+}
+
+// Создание блоков
+var pipe = [];
+
+pipe[0] = {
+ x : cvs.width,
+ y : 0
+}
+
+var score = 0;
+// Позиция птички
+var xPos = 10;
+var yPos = 150;
+var grav = 1.6;
+
+function draw() {
+ ctx.drawImage(bg, 0, 0);
+
+ for(var i = 0; i < pipe.length; i++) {
+ ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+ ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+ pipe[i].x--;
+
+ if(pipe[i].x == 60) {
+ pipe.push({
+ x : cvs.width,
+ y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+ });
+ }
+
+ // Отслеживание прикосновений
+ if(xPos + bird.width >= pipe[i].x
+ && xPos <= pipe[i].x + pipeUp.width
+ && (yPos <= pipe[i].y + pipeUp.height
+ || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+    //gameOver.play();
+    //sleep (800);
+    location.reload(); // Перезагрузка страницы
+ }
+
+ if(pipe[i].x == 5) {
+ score++;
+ score_audio.play();
+ }
+ }
+
+ ctx.drawImage(fg, 0, cvs.height - fg.height);
+ ctx.drawImage(bird, xPos, yPos);
+
+ yPos += grav;
+
+ ctx.fillStyle = "#fff";
+ ctx.font = "30px Verdana";
+ ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+
+ requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw;
